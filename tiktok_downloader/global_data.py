2e78f39c5c161979eeecc070d65ffe3adf237dab
X <work@Xs-MacBook-Pro.local>
2022-02-07 01:15:46 +0100

    new version

---
+++ tiktok_downloader/global_data.py
@@ -0,0 +1,39 @@
+# Directories
+DATA = "../data"
+IDS = "ids"
+LOG = "log"
+POSTS = "posts"
+VIDEOS = "videos"
+IMAGES = f"{DATA}/img"
+
+# Files
+POST_IDS = "post_ids.json"
+VIDEO_IDS = "video_ids.json"
+DATA_FILE = "data.json"
+LOG_FILE = "log.json"
+
+
+FILES = {
+            "data" : DATA,
+            "ids" : IDS,
+            "log" : LOG,
+            "posts" : POSTS,
+            "videos" : VIDEOS,
+            "images" : IMAGES,
+            "post_ids" : f"{DATA}/{IDS}/{POST_IDS}",
+            "video_ids" : f"{DATA}/{IDS}/{VIDEO_IDS}",
+            "data_file" : f"{DATA_FILE}",
+            "downloads" : [],
+            "logger" : f"{DATA}/{LOG}/{LOG_FILE}",
+        }
+
+
+
+# Commands
+tag = ""
+
+PARAMETERS = {
+            "scraper_attempts" : 3,
+#            "number_of_videos" : 3, # Number of videos to be downloaded by tiktok-scraper.
+            "sleep" : 8
+        }
---
+++ tiktok_downloader/hashtag_list.py
@@ -0,0 +1,7 @@
+hashtag_list = [
+# This is a sample hashtag list. Please enter your hashtag list (without the comment).
+            "london",
+            "paris",
+            "newyork",
+            "tokyo"
+        ]
