93930f647f5e564cd80d8ac64e44d25fbd32615d
johannawild <72805812+johannawild@users.noreply.github.com>
2022-05-02 23:53:51 +0200

    Delete hashtag_list_sample.py

--- tiktok_downloader/hashtag_list_sample.py
+++
@@ -1,8 +0,0 @@
-hashtag_list = [
-# This is a sample hashtag list. Please enter your hashtag list (without the comment).
-            "london",
-            "paris",
-            "newyork",
-            "tokyo"
-
-        ]
