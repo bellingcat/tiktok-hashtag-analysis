f8c12a8d689be91cead92d1f0be25b8bf62c674f
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-01-19 03:40:37 +0200

    Update hashtag_frequencies.py

--- tiktok_hashtag_analysis/hashtag_frequencies.py
+++ tiktok_hashtag_analysis/hashtag_frequencies.py
@@ -13,8 +13,8 @@ from typing import List, Tuple, Dict, Any
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import seaborn as sns
-from file_methods import check_file, check_existence
-from global_data import IMAGES, FILES
+from tiktok_hashtag_analysis.file_methods import check_file, check_existence
+from tiktok_hashtag_analysis.global_data import IMAGES, FILES
 
 warnings.filterwarnings("ignore", message="Glyph (.*) missing from current font")
 sns.set_theme(style="darkgrid")
