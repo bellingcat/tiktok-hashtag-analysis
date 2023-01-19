4c69f616e6f0ddbbd79ed792a1442335590dfdd9
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-01-19 03:26:38 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="tiktok-hashtag-analysis",
+    version="1.0.0",
+    author="Bellingcat",
+    packages=["tiktok_hashtag_analysis"],
+    description="Analyze hashtags within posts scraped from TikTok",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bellingcat/tiktok-hashtag-analysis",
+    license="MIT License",
+    install_requires=["seaborn", "matplotlib"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3'
+        ],
+    entry_points={
+        "console_scripts": [
+            "tiktok_hashtag_analysis=tiktok_hashtag_analysis.main:main",
+        ]
+    },
+)
