6b4ceaae61c949fc4a506a68512d516b236a53cf
msramalho <19508417+msramalho@users.noreply.github.com>
2023-03-13 09:54:06 +0000

    attempts at fixing CLI issues #18

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-import setuptools
+from setuptools import setup, find_packages
 from tiktok_hashtag_analysis.version import __version__
 
 with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
-setuptools.setup(
+setup(
     name="tiktok-hashtag-analysis",
     version=__version__,
     author="Bellingcat",
     author_email="tech@bellingcat.com",
     packages=["tiktok_hashtag_analysis"],
+    package_data={
+        "tiktok_hashtag_analysis": [
+            "logging.config",
+        ]
+    },
     description="Analyze hashtags within posts scraped from TikTok",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -22,10 +27,10 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3'
-        ],
+    ],
     entry_points={
         "console_scripts": [
-            "tiktok_hashtag_analysis=tiktok_hashtag_analysis.__main__:main",
+            "tiktok-hashtag-analysis=tiktok_hashtag_analysis.__main__:main",
         ]
     },
 )
